3d34cfa10de1b1f5b5287c7510875e698f8ff597
MohamedAjaf <84005978+MohamedAjaf@users.noreply.github.com>
2022-01-19 21:23:41 +0530

    Add files via upload

---
+++ LU Assignment-3.py
@@ -0,0 +1,12 @@
+name=input("Enter your Name: ")
+age=int(input("Enter your Age: "))
+gender=input("Enter your Gender: ")
+mt=input("Enter your Mother Tongue: ")
+country=input("Enter your country: ")
+aim=input("What is your aim in life: ")
+
+
+
+a=[name,age,gender,mt,aim, country]
+print('\n')
+print(f"Name:{a[0]} \nAge:{a[1]} \nGender:{a[2]} \nMother Tongue:{a[3]} \nCountry:{a[5]}\nAim: {a[4]}")
